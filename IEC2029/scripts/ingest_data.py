53647a87874d64fb8b73e61a3f6be6f367dcd093
thetomaunatlala-777 <thetomaunatlala@gmail.com>
2026-03-02 16:21:02 +0200

    mock data ingested and set up complete

--- IEC2029/scripts/ingest_data.py
+++ IEC2029/scripts/ingest_data.py
@@ -1,23 +1,32 @@
+import time
 import pandas as pd
 from sqlalchemy import create_engine, text
+from sqlalchemy.exc import OperationalError
 
-# Connect from Python container to Postgres container
 postgresql_url = 'postgresql://postgres:password@db:5432/IEC2029_Database'
-
 engine = create_engine(postgresql_url)
-connection = engine.connect()
 
-with engine.connect() as connection:
-    print("Connection successful!")
+# Wait until Postgres is ready
+for i in range(10):
+    try:
+        with engine.connect() as conn:
+            print("Connection successful!")
+            break
+    except OperationalError:
+        print("Postgres not ready, waiting 3s...")
+        time.sleep(3)
+else:
+    raise Exception("Could not connect to Postgres after 10 tries")
 
-    
-    connection.execute(text("CREATE SCHEMA IF NOT EXISTS bronze;"))
-    print("Schema 'bronze' is ready.")
+schemas = ['bronze', 'silver', 'gold']
 
-   
-    df = pd.read_csv('/app/data/National(Results Report - National).csv')  
-    print("CSV loaded successfully!")
 
-    
-    df.to_sql('users', con=connection, schema='bronze', if_exists='replace', index=False)
-    print("Data ingested into bronze.users successfully!")
+with engine.begin() as connection:  
+    for schema in schemas:
+        connection.execute(text(f"CREATE SCHEMA IF NOT EXISTS {schema};"))
+        print(f"Schema '{schema}' is ready.")
+
+    # Load CSV
+    df = pd.read_csv('/app/data/National(Results Report - National).csv')
+    df.to_sql('national_results', con=connection, schema='bronze', if_exists='replace', index=False)
+    print("Data ingested into bronze.national_results successfully!")
