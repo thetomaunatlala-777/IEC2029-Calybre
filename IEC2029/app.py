017c372b7ecb05b7d7d4ee7221eb2491c2cd3b5f
thetomaunatlala-777 <thetomaunatlala@gmail.com>
2026-03-02 10:29:21 +0200

    save before restart

--- IEC2029/app.py
+++ IEC2029/app.py
@@ -2,4 +2,4 @@
 import time
 while True:
     print("Python container is running...")
-    time.sleep(4)
+    time.sleep(4) #this delay allows the database container to start up before the application tries to connect to it, preventing connection errors during startup.
